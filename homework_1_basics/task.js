f0a51d703d2a1a42a981a7b704cae2df2f38a21d
alexandrabotvinova1406-del <alexandra.botvinova.1406@gmail.com>
2026-03-27 17:16:02 +0300

    update_task.js

--- homework_1_basics/task.js
+++ homework_1_basics/task.js
@@ -1,10 +1,40 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
+  const d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    const sqrtD = Math.sqrt(d);
+    const root1 = (-b + sqrtD) / (2 * a);
+    const root2 = (-b - sqrtD) / (2 * a);
+    arr.push(root1, root2);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthlyRate = percent / 100 / 12;
+  const loanBody = amount - contribution;
+
+  if (loanBody <= 0) {
+    return 0;
+  }
+
+  let monthlyPayment;
+  if (monthlyRate === 0) {
+    // Если ставка 0, платёж просто делится на месяцы
+    monthlyPayment = loanBody / countMonths;
+  } else {
+    monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+  }
+
+  const totalPayment = monthlyPayment * countMonths;
+  return Math.round(totalPayment * 100) / 100;
 }
